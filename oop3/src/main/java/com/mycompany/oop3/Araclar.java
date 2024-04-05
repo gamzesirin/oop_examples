ce78dd3a2ab797af58053f41eaf1b6eaceada933
Gamze Şirin <gamze1sirin@gmail.com>
2024-04-05 18:27:36 +0300

    oop works added.

---
+++ oop3/src/main/java/com/mycompany/oop3/Araclar.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oop3;
+
+/**
+ *
+ * @author gamze
+ */
+public class Araclar {
+    static class Binek{
+        static class Hashback{
+            public int Motorhacmi = 1600;
+        }
+         static class Sedan{
+            public int Motorhacmi = 2000;
+        }
+    }
+    static class Ticari{
+        static class Agir{
+            public String vergi="çok";
+        }
+         static class Orta{
+            public String vergi="orta";
+        }
+    }
+}
---
+++ oop3/src/main/java/com/mycompany/oop3/Oop3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop3;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop3 {
+
+    public static void main(String[] args) {
+      Araclar.Binek.Hashback hb1 = new Araclar.Binek.Hashback();
+        System.out.println(hb1.Motorhacmi);
+        
+        Araclar.Ticari.Orta orta1 = new Araclar.Ticari.Orta();
+        System.out.println(orta1.vergi);
+    }
+}
+//static sınıflar olduğu için nokta operatörü ile ulaşıyoruz.
