7269f6352d72002f20f7ba3c0c2948f619f108e6
Gamze Şirin <gamze1sirin@gmail.com>
2024-04-09 21:51:21 +0300

    oop works added.

---
+++ oop8/src/main/java/com/mycompany/oop8/Oop8.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop8;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop8 {
+
+    public static void main(String[] args) {
+       int n;
+       n=(int) (3.0*5);
+       
+       hesapla(5.6);           
+    }
+  
+    public static void hesapla(int x){
+           System.out.println(3);
+       }
+     public static void hesapla(double x){
+           System.out.println(3.0);
+       }
+     public static void hesapla(short x){
+           System.out.println("short");
+       }
+}
