eb6dcf3ab29768c6faa638d1bf11d300640e9e99
Gamze Şirin <gamze1sirin@gmail.com>
2024-04-09 21:51:40 +0300

    oop works added.

---
+++ oop9/src/main/java/com/mycompany/oop9/Oop9.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop9;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop9 {
+
+    public static void main(String[] args) {
+        hesapla(2,8);
+    }
+    public static void hesapla(String n ,int m ){
+        System.out.println(10);
+    }
+      public static void hesapla(int n ,int...m ){
+          System.out.println(3);
+    }
+        public static void hesapla(int n ,int m ){
+            System.out.println(5);
+        
+    }
+}
