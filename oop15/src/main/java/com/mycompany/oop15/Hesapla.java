915b424058f9e966afb0baece5ccf7ea5dc5e41d
Gamze Şirin <gamze1sirin@gmail.com>
2024-05-24 22:59:39 +0300

    oop works added.

---
+++ oop15/src/main/java/com/mycompany/oop15/Hesapla.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oop15;
+
+/**
+ *
+ * @author gamze
+ */
+public class Hesapla {
+    public void kare(int i)
+    {
+    i = i * i;
+        System.out.println("sayının karesi "+ i);
+    }
+}
---
+++ oop15/src/main/java/com/mycompany/oop15/Oop15.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop15;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop15 {
+
+    public static void main(String[] args) {
+        Hesapla nesne = new Hesapla();
+        int a = 20;
+        System.out.println(a);//20
+        nesne.kare(a);
+        System.out.println(a);//20
+    }
+}
+
