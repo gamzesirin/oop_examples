e51fe6e72d3af33f0bc4e3383409056c818a5f5a
Gamze Şirin <gamze1sirin@gmail.com>
2024-05-25 13:51:04 +0300

    oop works added.

---
+++ oop16/src/main/java/com/mycompany/oop16/Hesapla.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oop16;
+
+/**
+ *
+ * @author gamze
+ */
+public class Hesapla {
+    int i=20;
+    public void kare(Hesapla a)
+    {
+    a.i=a.i*a.i;//20*20=400
+    } 
+}
---
+++ oop16/src/main/java/com/mycompany/oop16/Oop16.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop16;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop16 {
+
+    public static void main(String[] args) {
+        Hesapla nesne = new Hesapla();
+        nesne.kare(nesne);
+        System.out.println(nesne.i);//400
+    }
+}
