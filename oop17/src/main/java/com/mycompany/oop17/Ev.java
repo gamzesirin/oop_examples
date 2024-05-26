8608c495a283cfde92998f7eee4420e3e0f2b0c6
Gamze Şirin <gamze1sirin@gmail.com>
2024-05-26 15:34:20 +0300

    oop works added.

---
+++ oop17/src/main/java/com/mycompany/oop17/Ev.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oop17;
+
+/**
+ *
+ * @author gamze
+ */
+public class Ev {
+    static int kapıNo=123;
+    String sokakAdı="Menekşe";
+    static void setKapı(int kapıNo)
+    {
+    Ev.kapıNo=kapıNo;
+    System.out.println(Ev.kapıNo);
+    }
+}
---
+++ oop17/src/main/java/com/mycompany/oop17/Oop17.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop17;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop17 {
+
+    public static void main(String[] args) {
+      System.out.println(Ev.kapıNo);//123
+      Ev.setKapı(321);//321
+    }
+}
