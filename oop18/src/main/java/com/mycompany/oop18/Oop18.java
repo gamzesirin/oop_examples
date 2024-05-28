84d6021f8183fe79b186eab3678386c5c1291921
Gamze Şirin <gamze1sirin@gmail.com>
2024-05-28 15:22:39 +0300

    oop works added.

---
+++ oop18/src/main/java/com/mycompany/oop18/Oop18.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop18;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop18 {
+
+    static double oran = 3.5;
+    static double sonuc; // Bu değer artık sınıf seviyesinde tanımlandı
+
+    public static void main(String[] args) {
+        int sayac = 1;
+        sonuc = oran * sayac;
+       
+        System.out.println(sonuc);
+    }
+}
