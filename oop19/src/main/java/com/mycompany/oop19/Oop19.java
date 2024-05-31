390b8d6d2c8c742294b570a6b07f65c06d208305
Gamze Şirin <gamze1sirin@gmail.com>
2024-05-31 09:41:11 +0300

    oop works added.

---
+++ oop19/src/main/java/com/mycompany/oop19/Oop19.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop19;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop19 {
+
+    int i;
+    {
+        i=5;
+        System.out.println(i);
+    }
+    public static void main(String[] args) {
+     Oop19 o = new Oop19();
+     
+    }
+}
