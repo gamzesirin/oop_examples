d14433a00c781f5b20251c22b1f20255dc1685cd
Gamze Şirin <gamze1sirin@gmail.com>
2024-06-01 10:27:50 +0300

    oop works added.

---
+++ Oop20/src/main/java/com/mycompany/oop20/Oop20.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop20;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop20 {
+    
+    static int m;
+    static{
+        m=0;
+        System.out.println(m);
+    }
+    public static void main(String[] args) {
+       
+    }
+}
