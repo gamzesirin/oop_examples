c5b65aeb02b8023faaf876e6b017d6431b2f02d3
Gamze Şirin <gamze1sirin@gmail.com>
2024-06-02 21:09:08 +0300

    oop works added.

---
+++ oop21/src/main/java/com/mycompany/oop21/Oop21.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop21;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop21 {
+int i;
+{
+    i=5;
+    System.out.println(i);
+}
+static int m;
+static{
+    m=0;
+    System.out.println(m);
+}
+    public static void main(String[] args) {
+       
+    }
+}
