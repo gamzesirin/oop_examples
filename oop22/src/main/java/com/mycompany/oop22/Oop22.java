2e8aa54661767b9a46cdffa517287f6acc9b7030
Gamze Şirin <gamze1sirin@gmail.com>
2024-06-05 09:50:14 +0300

    added oop works

---
+++ oop22/src/main/java/com/mycompany/oop22/Oop22.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop22;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop22 {
+int i;
+{
+    i=5;
+    System.out.println(i);
+}
+static int m;
+static{
+    m=0;
+    System.out.println(m);
+}
+    public static void main(String[] args) {
+       Oop22 o = new Oop22();
+         Oop22 o1 = new Oop22();
+           Oop22 o2 = new Oop22();
+    }
+}
