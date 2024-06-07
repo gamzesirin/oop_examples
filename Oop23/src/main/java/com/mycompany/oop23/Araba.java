47cb87f499b7b2b2e62041dc0b80f230e3b99ddf
Gamze Şirin <gamze1sirin@gmail.com>
2024-06-07 17:49:15 +0300

    oop works added.

---
+++ Oop23/src/main/java/com/mycompany/oop23/Araba.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oop23;
+
+/**
+ *
+ * @author gamze
+ */
+public class Araba {
+    public String marka;
+    public void yaz(){
+        String marka="Bmv";
+        this.marka="Ferrari";
+        System.out.println("marka : "+ this.marka);
+        System.out.println("marka : "+ marka);
+
+    }
+    
+}
---
+++ Oop23/src/main/java/com/mycompany/oop23/Oop23.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop23;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop23 {
+
+    public static void main(String[] args) {
+        Araba a = new Araba();
+        a.yaz();
+        System.out.println("marka : "+a.marka);
+    }
+}
