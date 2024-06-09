57623e8264f2cce22ce24c3fa2808d20e6f84026
Gamze Şirin <gamze1sirin@gmail.com>
2024-06-09 08:29:58 +0300

    added.

---
+++ oop26/src/main/java/com/mycompany/oop26/MyClass.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oop26;
+
+/**
+ *
+ * @author gamze
+ */
+public class MyClass {
+    static {
+        System.out.println("1.blok çalıştı");
+    }
+    public MyClass(){
+        System.out.println("2. kurucu metot çalıtı ");
+    }
+    {
+        System.out.println("3. init blok çalıştı");
+    }
+}
---
+++ oop26/src/main/java/com/mycompany/oop26/Oop26.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop26;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop26 {
+
+    public static void main(String[] args) {
+     MyClass obj = new MyClass();
+    }
+}
