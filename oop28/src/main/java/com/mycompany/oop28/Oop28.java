50e845bf72aee4bbb69630893ad399334bcc5b67
Gamze Şirin <gamze1sirin@gmail.com>
2024-06-11 16:52:25 +0300

    added.

---
+++ oop28/src/main/java/com/mycompany/oop28/Oop28.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oop28;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oop28 {
+
+    public static void main(String[] args) {
+        System.out.println(MyClass.x);
+    }
+}
