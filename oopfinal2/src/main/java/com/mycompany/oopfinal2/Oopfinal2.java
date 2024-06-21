7a6355a9b5568c49d86a3e5f5d4b7422ae5d8a8a
Gamze Şirin <155438363+gamzesirin@users.noreply.github.com>
2024-06-21 22:18:49 +0300

    added.

---
+++ oopfinal2/src/main/java/com/mycompany/oopfinal2/Oopfinal2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oopfinal2;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oopfinal2 {
+
+    public static void main(String[] args) {
+        String str="merhaba dünya";
+        str=str.replace("merhaba","selam");
+        System.out.println(str);
+    }
+}
---
+++ oopfinal3/src/main/java/com/mycompany/oopfinal3/JavaBean.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oopfinal3;
+
+/**
+ *
+ * @author gamze
+ */
+
+
+//IMMUTABLE SINIF OLUŞTURDUM 
+public final class JavaBean {
+    private final String name;
+   
+    public JavaBean(String name){
+        this.name=name;
+    }
+    public String getName(){
+        return name;
+    }
+  
+}
+
+
+
+
