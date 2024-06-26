1ba5ad9cc82db173270cd852d19592aa5bf1d197
Gamze Şirin <155438363+gamzesirin@users.noreply.github.com>
2024-06-26 12:53:27 +0300

    added oop works

---
+++ oopfinal6/src/main/java/com/mycompany/oopfinal6/Oopfinal6.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oopfinal6;
+
+/**
+ *
+ * @author gamze
+ */
+public class Oopfinal6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+// muhakkak inherit edilmesi gereken sınıflar abstract tanımlanır 
+
+
+
+
+
+//Bileşik Nesneye ve tekil nesneye  bak sınav için
+
+
+
+// protected => çocukların erişebileceği bir yapı oluşturmak için kullanılır
+
+//static final olursa bir metod biz bunu gizleyemeyiz
+
+
+
+
+
+//enumda önce dinamikler sınra staticler çalışır static bir kere çalışır => Sınav
+
+//  override ve overload farkı sınav 
+
+//büyük türü küçük türeçevirme bilinçli cats işlemitersi bilinçsiz cast işlemi 
+
+// kaplumbağa ve kedi beslen metodunu overrride etti ilk örnek upcasting 
+
+
+//hayvan hayvan 1 = new KEDİ => upcasting oluyor 
+
+//kedi kedi 1 = (KEDİ) HAYVAN HAYVAN  => DOWN CASTİNG
+
+
